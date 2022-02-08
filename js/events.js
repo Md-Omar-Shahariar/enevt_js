b6c12c1bb1d8d85c45283a67c699c08e2b926854
Md-Omar-Shahariar <osafridi00@gmail.com>
2022-02-08 11:03:14 +0600

    add eventlistner

--- js/events.js
+++ js/events.js
@@ -1,14 +1,27 @@
 function makeRed() {
-    document.body.style.backgroundColor = "REd";
-  }
+document.body.style.backgroundColor = "REd";
+}
 const blueButton = document.getElementById("blue-button")
 blueButton.onclick = makeBlue
 
 function makeBlue(){
-        document.body.style.backgroundColor = "blue"
-    }
+document.body.style.backgroundColor = "blue"
+}
 const greenButton = document.getElementById("make-green")
 
 greenButton.onclick = function (){
-        document.body.style.backgroundColor= "green"
-    }
+document.body.style.backgroundColor= "green"
+}
+
+//Event Listener
+const goldenRod = document.getElementById('make-goldenrod')
+goldenRod.addEventListener('click', makeGoldenRod)
+
+function makeGoldenRod(){
+document.body.style.backgroundColor = 'goldenrod'
+}
+// addEventListener
+const hotPink = document.getElementById('make-hotpink')
+hotPink.addEventListener('click', function(){
+document.body.style.backgroundColor = 'hotpink'
+})      
